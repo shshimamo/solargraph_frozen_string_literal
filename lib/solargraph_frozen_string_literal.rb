4a496206cacf8b12730d7045d8402366ce5d9ef5
hiroya.shimamoto <shshimamo@gmail.com>
2022-09-11 20:10:48 +0900

    Add FrozenStringReporter

--- lib/solargraph_frozen_string_literal.rb
+++ lib/solargraph_frozen_string_literal.rb
@@ -1,8 +1,17 @@
-# frozen_string_literal: true
+require 'solargraph'
 
-require_relative "solargraph_frozen_string_literal/version"
-
-module SolargraphFrozenStringLiteral
-  class Error < StandardError; end
-  # Your code goes here...
+class FrozenStringReporter < Solargraph::Diagnostics::Base
+  def diagnose source, _api_map
+    return [] if source.code.empty? || source.code.start_with?('# frozen_string_literal:')
+    [
+      {
+        range: Solargraph::Range.from_to(0, 0, 0, source.code.lines[0].length),
+        severity: Diagnostics::Severities::WARNING,
+        source: 'FrozenString',
+        message: 'File does not start with frozen_string_literal.'
+      }
+    ]
+  end
 end
+
+Solargraph::Diagnostics.register 'frozen-string', FrozenStringReporter
