bddefd21f04a9ba72514ae0ca6b1dd3da62bfb9c
hiroya.shimamoto <shshimamo@gmail.com>
2022-09-11 19:55:22 +0900

    first commit

---
+++ lib/solargraph_frozen_string_literal.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "solargraph_frozen_string_literal/version"
+
+module SolargraphFrozenStringLiteral
+  class Error < StandardError; end
+  # Your code goes here...
+end
